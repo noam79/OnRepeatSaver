dbe9ed0ef70197ceb61b11d516a1186917138d1c
noam79 <noameilon8@gmail.com>
2023-05-20 19:28:26 +0300

    Refactor

---
+++ OnRepeatSaver/ConfigurationPaths.cs
@@ -0,0 +1,7 @@
+﻿namespace OnRepeatSaver;
+
+public static class ConfigurationPaths
+{
+    public const string Credentials = "credentials.json";
+    public const string ClientId = "client_id.txt";
+}
---
+++ OnRepeatSaver/SongCopier.cs
@@ -0,0 +1,24 @@
+﻿using SpotifyAPI.Web;
+
+namespace OnRepeatSaver;
+
+public class SongCopier
+{
+    public static async Task CopySongsAsync(SpotifyClient client, string sourcePlaylistId, string targetPlaylistId)
+    {
+        // TODO: only add distinct songs to avoid duplications.
+        
+        var sourcePlaylistSongs = await client.PaginateAll(await client.Playlists.GetItems(sourcePlaylistId));
+        var targetPlaylistSongs = await client.PaginateAll(await client.Playlists.GetItems(targetPlaylistId));
+
+        var songsToAdd = targetPlaylistSongs.Except(sourcePlaylistSongs);
+
+        var addPlaylistItemsRequest = new PlaylistAddItemsRequest(
+            songsToAdd.Select(playlistTrack => ((FullTrack)playlistTrack.Track).Uri
+            ).ToList());
+
+        await client.Playlists.AddItems(
+            targetPlaylistId,
+            addPlaylistItemsRequest);
+    }
+}
