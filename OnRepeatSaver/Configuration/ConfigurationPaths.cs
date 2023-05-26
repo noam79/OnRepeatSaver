d6295af5486512f6cf64c49aed78b2c60eb85e0f
noam79 <noameilon8@gmail.com>
2023-05-26 20:51:29 +0300

    Removed old project dir

--- OnRepeatSaver/Configuration/ConfigurationPaths.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace OnRepeatSaver.Configuration;
-
-public static class ConfigurationPaths
-{
-    public const string Credentials = "Configuration/credentials.json";
-    public const string ClientId = "Configuration/client_id.txt";
-}
