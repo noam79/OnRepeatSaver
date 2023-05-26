ce5985fecd6ccb896f9a3174cf2aa8f31cc536c6
noam79 <noameilon8@gmail.com>
2023-05-26 20:36:25 +0300

    Fixed paths

--- OnRepeatSaver/Configuration/ConfigurationPaths.cs
+++ OnRepeatSaver/Configuration/ConfigurationPaths.cs
@@ -2,6 +2,6 @@
 
 public static class ConfigurationPaths
 {
-    public const string Credentials = "/Configuration/credentials.json";
-    public const string ClientId = "/Configuration/client_id.txt";
+    public const string Credentials = "Configuration/credentials.json";
+    public const string ClientId = "Configuration/client_id.txt";
 }
